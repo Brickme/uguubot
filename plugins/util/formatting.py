d7057e1dc0b3a6323e90737eb93e72b573b5d693
Scott Steiner <nothingfinerthanscottsteiner@gmail.com>
2016-10-29 18:36:22 -0400

    Update formatting.py to prevent nick spamming

--- plugins/util/formatting.py
+++ plugins/util/formatting.py
@@ -1,3 +1,6 @@
+import re
+import channel
+
 def filesize(num, suffix='B'):
     for unit in ['','Ki','Mi','Gi','Ti','Pi','Ei','Zi']:
         if abs(num) < 1024.0:
@@ -5,5 +8,16 @@ def filesize(num, suffix='B'):
         num /= 1024.0
     return '{0:.1f}{}{}'.format(num, 'Yi', suffix)
 
-def output(scriptname, fields=[], color=11):
-    return u'[{}] {}'.format(scriptname, ' - '.join(fields))
+def output(db, chan, scriptname, fields=[], color=11):
+    split_output = set(re.sub('[#~&@+%,\.]', '', ' '.join(fields).lower()).split(' '))
+    users = set(channel.users(db, chan))
+    match = split_output & users
+    if scriptname is False:
+        scriptname = ''
+    else:
+        scriptname = '[{}] '.format(scriptname)
+
+    if len(match) >= 3:
+        print(u'Nick spam detected. Output ignored: [{}] {}'.format(scriptname, ' - '.join(fields)))
+    else:
+        return u'{}{}'.format(scriptname, ' - '.join(fields))
