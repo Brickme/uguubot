6d7c20ee702ba6fa1654354141940bdd714ff86e
Scott Steiner <nothingfinerthanscottsteiner@gmail.com>
2016-12-15 00:32:58 -0500

    Beginnings of item store. Doesnt save items in inventory yet

--- plugins/goodboy.py
+++ plugins/goodboy.py
@@ -1,7 +1,7 @@
 #########################################################################################
 # Name			Good Boy Points
 # Description		Gives the user good boy points for tendies and McRibs
-# Version		1.1.1 (2016-12-14)
+# Version		1.1.2 (2016-12-14)
 # Contact		ScottSteiner@irc.rizon.net
 # Website		https://github.com/ScottSteiner/uguubot
 # Copyright		2016, ScottSteiner <nothingfinerthanscottsteiner@gmail.com>
@@ -17,6 +17,11 @@ phrases = {
 	'mediocre': ['You didn\'t do anything to earn good boy points!'],
 	'bad': ['You were a bad boy!  You lost {} good boy points!'], 
 	'awful': ['REEEEEEEEEEEEEEEEEEEEEEEEE. GET OUT NORMIE AND LEAVE {} GOOD BOY POINTS BEHIND']
+}
+
+items = {
+	'tendies': {'price': 10, 'bonus': 0.05, 'description': 'Mommy\'s homemade tendies'},
+	'piss bottle': {'price': 50, 'bonus': 0.25, 'description': 'A 2 liter mountain dew bottle so you never have to leave the room while shitposting on pedochan.'}
 }
 
 @hook.command('piss', autohelp=False)
@@ -44,3 +49,38 @@ def behave(inp, nick=None, db=None, input=None):
 	database.set(db,'users','gbp',unicode(current),'nick',nick)
 	return formatting.output(db, input.chan, 'Good Boy Points', [phrase.format(abs(result)), 'You now have {} good boy points!'.format(current)])
 
+@hook.command()
+def buy(inp, nick=None, db=None, input=None, notice=None):
+	"buy -- buy special presents with your good boy points"
+
+	global items
+	nick = nick.lower()
+	current = database.get(db,'users','gbp','nick',nick) or 0
+	current = int(current)
+
+	try:
+		item = ' '.join(input.inp_unstripped.split()[1:])
+		quantity = int(input.inp_unstripped.split()[0])
+	except:
+		item = inp
+		quantity = 1
+	print(item)
+
+	if item not in items:
+		notice(formatting.output(db, input.chan, 'Good Boy Points', ['Item not available in store.']))
+	else:
+		price = items[item]['price'] * quantity
+		if current >= price:
+			balance = current - price
+			database.set(db,'users','gbp',unicode(balance),'nick',nick)
+			return formatting.output(db, input.chan, 'Good Boy Points', ['You purchased {} {} for {} good boy points.'.format(quantity, item, price), 'You now have {} good boy points!'.format(balance)])
+		else:
+			return formatting.output(db, input.chan, 'Good Boy Points', ['You can\'t afford that!  You have {} good boy points but need {} to buy {} {}!!'.format(current, price, quantity, item)])
+
+@hook.command(autohelp=False)
+def balance(inp, nick=None, db=None, input=None, notice=None):
+	"balance -- check your good boy points balance"
+
+	nick = nick.lower()
+	current = database.get(db,'users','gbp','nick',nick) or 0
+	return formatting.output(db, input.chan, 'Good Boy Points', ['You currently have {} good boy points.'.format(current)])
