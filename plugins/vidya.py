a029acb0b8670bf3b43ed6a1ed9892cb20f3ec9e
Scott Steiner <nothingfinerthanscottsteiner@gmail.com>
2016-12-31 04:13:41 -0500

    Initial release of gameinfo command

---
+++ plugins/vidya.py
@@ -0,0 +1,66 @@
+#########################################################################################
+# Name			Video Game Info
+# Description		Information about video games
+# Version		1.0 (2016-12-30)
+# Contact		ScottSteiner@irc.rizon.net
+# Website		https://github.com/ScottSteiner/uguubot
+# Copyright		2016, ScottSteiner <nothingfinerthanscottsteiner@gmail.com>
+# License		GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
+#########################################################################################
+
+from util import hook, http, formatting
+from lxml import html
+import decimal
+
+@hook.command(autohelp=True)
+def gameinfo(inp, db=None, input=None):
+	'gameinfo -- Gets steam game information'
+
+	appid = app_name_to_id(inp)
+
+	data = game_info(appid)
+
+	name = data['name']
+	developer = ', '.join(data['developers']) or []
+	description = data['short_description']
+	player_count = '{:,} current players'.format(current_players(appid))
+
+	free = data['is_free']
+	if free == True:
+		price = 'Free To Play'
+	else:
+		current_price = data['price_overview']['final'] or 0
+		current_price = decimal.Decimal(current_price) / 100
+		initial_price = data['price_overview']['initial'] or 0
+		initial_price = decimal.Decimal(initial_price) / 100
+		discount = data['price_overview']['discount_percent']
+
+		if current_price == initial_price: price = '${}'.format(current_price)
+		else:				   price = '${} (${}, {}% off)'.format(current_price, initial_price, discount)
+
+	output = [name,developer,price, player_count, description]
+	return formatting.output(db, input.chan, 'Game Info', output)
+
+def current_players(appid):
+	api_url = 'https://api.steampowered.com/ISteamUserStats/GetNumberOfCurrentPlayers/v1'
+
+	try:
+		data = http.get_json(api_url, appid=appid)
+		count = data['response']['player_count']
+		return count
+	except Exception as e:
+		return e
+
+def app_name_to_id(appname):
+	search_url = 'http://store.steampowered.com/search/'
+
+	body = http.get(search_url, term=appname)
+	body = html.fromstring(body)
+	id = body.xpath('//a[contains(@class, "search_result_row")]/@data-ds-appid')[0]
+	return id
+
+def game_info(appid):
+	api_url = 'http://store.steampowered.com/api/appdetails'
+
+	data = http.get_json(api_url, appids=appid)[appid]['data']
+	return data
