1de0163a90e75c3d1682823ecc91c4b977d60506
Scott Steiner <nothingfinerthanscottsteiner@gmail.com>
2017-05-19 01:04:07 -0400

    HTML format for allhashes. hashtag values are now in a dict to add more data

--- plugins/hashtags.py
+++ plugins/hashtags.py
@@ -3,6 +3,9 @@ from util import hook, http, text, execute, web
 import string
 import sqlite3
 import re
+import time
+import json
+from datetime import datetime
 
 re_lineends = re.compile(r'[\r\n]*')
 
@@ -15,6 +18,9 @@ shortcodes = {
 '[i]': '\x16',
 '[/i]': '\x16'}
 
+html_body = u"<html><head><title>{}</title></head><body>{}</body></html>"
+html_lines = u"<b>{}</b>: {}<br/>\n"
+html_items = u"<span style='border:1px solid {border}; background-color:{bg};' title='Added by {nick} on {date}'>{value}</span>"
 
 def db_init(db):
     db.execute("create table if not exists mem(word, data, nick,"
@@ -26,8 +32,15 @@ def get_memory(db, word):
 
     row = db.execute("select data from mem where word=lower(?)",
                      [word]).fetchone()
+
     if row:
-        return row[0]
+        result = row[0]
+        try: result = json.loads(result)
+        except: pass
+        if isinstance(result, list) == False:
+            #Converts old style to dict
+            result = [{'value': result, 'nick': None, 'date': None}]
+        return result
     else:
         return None
 
@@ -40,44 +53,23 @@ def remember(inp, nick='', db=None, say=None, input=None, notice=None):
     "remember <word> <data> -- Remembers <data> with <word>."
     db_init(db)
 
-    append = False
-
     try:
         word, data = inp.split(None, 1)
-        #word = inp.group(1)
-        #data = inp.group(2)
     except ValueError:
         notice(remember.__doc__)
+    time_now = time.time()
 
     old_data = get_memory(db, word)
 
-    #if data.startswith('+') or data.find('also') and old_data:
-    if old_data:
-        append = True
-        # remove + symbol
-        new_data = data.replace('+','')
-        #new_data = data[1:]
-        # append new_data to the old_data
-        if len(new_data) > 1 and new_data[1] in (string.punctuation + ' '):
-            data = old_data + new_data
-        else:
-            data = old_data + ' and ' + new_data
+    if old_data: new_data = old_data
+    else: new_data = []
+    new_data.append({'value': data, 'nick': input.nick, 'date': time_now})
 
-    db.execute("replace into mem(word, data, nick) values"
-               " (lower(?),?,?)", (word, data.replace('<py>',''), nick))
+    db.execute("replace into mem(word, data, nick) values (lower(?),?,?)", (word, json.dumps(new_data), nick))
     db.commit()
 
-    if old_data:
-        if append:
-            notice("Appending \x02%s\x02 to \x02%s\x02" % (new_data, old_data))
-        else:
-            notice('Remembering \x02%s\x02 for \x02%s\x02. Type ?%s to see it.'
-                % (data, word, word))
-            notice('Previous data was \x02%s\x02' % old_data)
-    else:
-        notice('Remembering \x02%s\x02 for \x02%s\x02. Type ?%s to see it.'
-                % (data, word, word))
-
+    if old_data: notice("Appending \x02{}\x02 to \x02{}\x02".format(data, word))
+    else: notice('Remembering \x02{}\x02 for \x02{}\x02. Type ?{} to see it.'.format(data, word, word))
 
 @hook.command("f", adminonly=True)
 @hook.command(adminonly=True)
@@ -91,12 +83,11 @@ def forget(inp, db=None, input=None, notice=None):
         db.execute("delete from mem where word=lower(?)",
                    [inp])
         db.commit()
-        notice('"%s" has been forgotten.' % data.replace('`', "'"))
+        notice('"%s" has been forgotten.' % inp.replace('`', "'"))
         return
     else:
         notice("I don't know about that.")
         return
-
 
 @hook.command
 def info(inp, notice=None, db=None):
@@ -105,10 +96,17 @@ def info(inp, notice=None, db=None):
     db_init(db)
 
     # attempt to get the factoid from the database
-    data = get_memory(db, inp.strip())
+    data = db.execute("SELECT data FROM mem where word=lower(?)", [inp]).fetchone()[0]
 
     if data:
-        notice(data)
+        data = json.loads(data)
+        for row in data:
+            try: date = datetime.fromtimestamp(row['date']).strftime("%F %T%Z")
+            except: date = 'N/A'
+            nick = row['nick']
+            value = row['value']
+            print('{}: {} ({} on {})'.format(inp.strip(), value, nick, date))
+        notice('{}: {} ({} on {})'.format(inp.strip(), value, nick, date))
     else:
         notice("Unknown Factoid.")
 
@@ -124,44 +122,31 @@ def hashtag(inp, say=None, db=None, bot=None, me=None, conn=None, input=None):
 
     db_init(db)
 
+    output = []
+
     # split up the input
     split = inp.group(1).strip().split(" ")
     factoid_id = split[0]
 
-    if len(split) >= 1:
-        arguments = " ".join(split[1:])
-    else:
-        arguments = ""
-
     data = get_memory(db, factoid_id)
+    if data is None: return
 
-    if data:
-        # factoid preprocessors
-        if data.startswith("<py>"):
-            code = data[4:].strip()
-            variables = 'input="""%s"""; nick="%s"; chan="%s"; bot_nick="%s";' % (arguments.replace('"', '\\"'),
-                         input.nick, input.chan, input.conn.nick)
-            result = execute.eval_py(variables + code)
-        elif data.startswith("<url>"):
-            url = data[5:].strip()
-            try:
-                result = http.get(url)
-            except http.HttpError:
-                result = "Could not fetch URL."
-        else:
-            result = data
+    for key in data:
+        result = key['value']
 
         # factoid postprocessors
         result = text.multiword_replace(result, shortcodes)
+        result = result.replace('http://a.pomf.se','https://web.archive.org/web/20150612013823/a.pomf.se')
 
         if result.startswith("<act>"):
             result = result[5:].strip()
             me(result)
         else:
             if prefix_on:
-                say("\x02[%s]:\x02 %s" % (factoid_id, result))
+                output.append("\x02[%s]:\x02 %s" % (factoid_id, result))
             else:
-                say("\x02%s\x02 %s" % (factoid_id, result))
+                output.append(result)
+    return ', '.join(output)
 
 @hook.command(r'keys', autohelp=False)
 @hook.command(r'key', autohelp=False)
@@ -178,4 +163,51 @@ def hashes(inp, say=None, db=None, bot=None, me=None, conn=None, input=None):
     if rows:
 	url = web.isgd(web.haste(", ".join(tuple(x[0] for x in rows))))
 	return "{}: {}".format(url, ", ".join(tuple(x[0] for x in rows)))
+    else: return "No results."
+
+@hook.command(r'allkeys', autohelp=False, adminonly=True)
+@hook.command(autohelp=False, adminonly=True)
+def allhashes(inp, say=None, db=None):
+    "allhashes -- Shows hash names for all known hashes."
+
+    search = "SELECT word, data, nick FROM mem"
+    if inp: search = "{} WHERE word LIKE '%{}%'".format(search, inp)
+    search = "{} ORDER BY word".format(search)
+
+    rows = db.execute(search).fetchall()
+
+    if rows:
+	web_list = []
+	irc_list = []
+	for row in rows:
+		formatted_values = []
+		try:
+			values = json.loads(row[1])
+		except:
+			values = [row[1]]
+		for value in values:
+			if type(value) is not dict:
+				value = {'value': value, 'date': 'N/A', 'nick': row[2]}
+				background_color = 'palegoldenrod'
+				border_color = 'darkgoldenrod'
+				#db.execute("replace into mem(word, data, nick) values (lower(?),?,?)", (row, value, nick))
+				#db.commit()
+
+			else:
+				background_color = 'aqua'
+				border_color = 'deepskyblue'
+			date = value['date']
+			try:
+				date = datetime.fromtimestamp(date).strftime("%F %T%Z")
+			except:
+				date = 'N/A'
+			nick = value['nick']
+			value = value['value']
+			formatted_values.append(html_items.format(border=border_color, bg=background_color, value=value, nick=nick, date=date))
+		formatted_values = ' '.join(formatted_values)
+		web_list.append(html_lines.format(row[0], formatted_values))
+	output_web = html_body.format('rms hashtags', ''.join(web_list))
+	url = web.pomf({'hashes.html': output_web})['hashes.html']
+	url = web.isgd(url)
+	return url
     else: return "No results."
