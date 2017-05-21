b997c95f27be2a1d3f7f259d1faf478e28e078e8
Scott Steiner <nothingfinerthanscottsteiner@gmail.com>
2017-05-21 19:03:42 -0400

    Standardized formatting

--- plugins/youtube.py
+++ plugins/youtube.py
@@ -1,7 +1,7 @@
 import re
 import time
 
-from util import hook, http, timeformat
+from util import hook, http, timeformat, formatting
 
 youtube_re = (r'(?:youtube.*?(?:v=|/v/)|youtu\.be/|yooouuutuuube.*?id=)'
               '([-_a-zA-Z0-9]+)', re.I) 
@@ -9,7 +9,7 @@ youtube_re = (r'(?:youtube.*?(?:v=|/v/)|youtu\.be/|yooouuutuuube.*?id=)'
 base_url = 'https://www.googleapis.com/youtube/v3/'
 search_api_url = base_url + 'search?part=id,snippet'
 api_url = base_url + 'videos?part=snippet,statistics,contentDetails'
-video_url = "http://youtu.be/%s"
+video_url = "http://youtu.be/{}"
 
 cache = {}
 
@@ -17,7 +17,7 @@ def plural(num=0, text=''):
     return "{:,} {}{}".format(num, text, "s"[num==1:])
 
 
-def get_video_description(key,video_id):
+def get_video_description(db, chan, key, video_id):
     request = http.get_json(api_url, key=key, id=video_id)
 
     if request.get('error'):
@@ -27,81 +27,75 @@ def get_video_description(key,video_id):
 
     title = re.sub(r'[\r\n]+', ' ', data['snippet']['title'])
 
-    try:
-        data['contentDetails'].get('duration')
-    except KeyError:
-        return u'\x02{}\x02'.format(title)
+    if 'duration' in data['contentDetails'].keys():
+        length = data['contentDetails']['duration']
+        timelist = re.findall('(\d+[DHMS])', length)
 
-    length = data['contentDetails']['duration']
-    timelist = re.findall('(\d+[DHMS])', length)
+        length = 0
+        for t in timelist:
+            t_field = int(t[:-1])
+            if   t[-1:] == 'D': length += 86400 * t_field
+            elif t[-1:] == 'H': length += 3600 * t_field
+            elif t[-1:] == 'M': length += 60 * t_field
+            elif t[-1:] == 'S': length += t_field
 
-    length = 0
-    for t in timelist:
-        t_field = int(t[:-1])
-        if   t[-1:] == 'D': length += 86400 * t_field
-        elif t[-1:] == 'H': length += 3600 * t_field
-        elif t[-1:] == 'M': length += 60 * t_field
-        elif t[-1:] == 'S': length += t_field
-
-    if length > 86400: length=time.strftime("%d:%H:%M:%S", time.gmtime(length))
-    elif length > 3600: length=time.strftime("%H:%M:%S", time.gmtime(length))
-    else: length=time.strftime("%M:%S", time.gmtime(length))
+        if length > 86400: length=time.strftime("%d:%H:%M:%S", time.gmtime(length))
+        elif length > 3600: length=time.strftime("%H:%M:%S", time.gmtime(length))
+        else: length=time.strftime("%M:%S", time.gmtime(length))
 
     uploader = data['snippet']['channelTitle']
 
-    out = u'\x02\x0301,00You\x0300,04Tube\017 \x02{}\x02 ({}) by \x02{}\x02.'.format(title, length, uploader)
+    out = [u'\x02{}\x02 ({}) by \x02{}\x02'.format(title, length, uploader)]
 
-    try:
-        data['statistics']
-    except KeyError:
-        return out
+    if 'statistics' in data.keys():
+        stats = data['statistics']
+        likes = plural(int(stats['likeCount']), "like")
+        dislikes = plural(int(stats['dislikeCount']), "dislike")
 
-    stats = data['statistics']
-    likes = plural(int(stats['likeCount']), "like")
-    dislikes = plural(int(stats['dislikeCount']), "dislike")
+        ratingcount = int(stats['likeCount']) + int(stats['dislikeCount'])
+        try:
+            percent = (100 * float(stats['likeCount']))/ratingcount
+            out.append(u'{:.1f}% ({:,} rating{})'.format(percent, ratingcount, "s"[ratingcount==1:]))
+        except:
+            out.append(u'{:,} rating{}'.format(ratingcount, "s"[ratingcount==1:]))
 
-    ratingcount = int(stats['likeCount']) + int(stats['dislikeCount'])
-    try:
-        percent = 100 * int(stats['likeCount'])/ratingcount
-        out += u' {:.1f}% ({:,} rating{})'.format(percent, ratingcount, "s"[ratingcount==1:])
-    except:
-        out += u' {:,} rating{}'.format(ratingcount, "s"[ratingcount==1:])
+        views = int(stats['viewCount'])
 
-    views = int(stats['viewCount'])
+
+        if 'commentCount' in stats:
+            comments = int(stats['commentCount'])
+        else:
+            comments = 0
+
+        if comments > 0:
+            out.append(u'\x02{:,}\x02 comment{}'.format(comments, "s"[comments==1:]))
+        if views > 0:
+            out.append(u'\x02{:,}\x02 view{}'.format(views, "s"[views==1:]))
 
     upload_time = time.strptime(data['snippet']['publishedAt'], "%Y-%m-%dT%H:%M:%S.000Z")
+    out.append(u'uploaded \x02{}\x02'.format(time.strftime("%Y-%m-%d", upload_time)))
 
-    comments = int(stats['commentCount'])
+    if 'contentRating' in data['contentDetails'].keys():
+        out.append(u'\x034NSFW\x02')
 
-    out += u'. \x02{:,}\x02 comment{} and \x02{:,}\x02 view{} since \x02{}\x02 - \037http://youtu.be/{}\037'.format(comments, "s"[comments==1:], views, "s"[views==1:], time.strftime("%Y-%m-%d", upload_time), video_id)
-
-    try:
-        data['contentDetails']['contentRating']
-    except KeyError:
-        return out
-
-    out += u' - \x034NSFW\x02'
-
-    return out
+    return formatting.output(db, chan, u'\x02\x0301,00You\x0300,04Tube\017', out)
 
 
 @hook.regex(*youtube_re)
-def youtube_url(match,bot=None):
+def youtube_url(match,bot=None,chan=None,input=None,db=None):
     match = match.group(1)
     if match in cache:
-      print('{} found in {}'.format(match, cache))
+      print('{} found in YouTube cache.'.format(match))
       if (time.time() < (cache[match] + 60)): return
-    else:
-      print('{} not found in {}'.format(match, cache))
     cache[match] = time.time()
 
     key = bot.config.get("api_keys", {}).get("google")
-    return get_video_description(key,match)
+    return get_video_description(db,input.chan,key,match)
 
 
 @hook.command('yt')
 @hook.command
-def youtube(inp, bot=None):
+def youtube(inp, bot=None, chan=None, input=None, db=None):
     """youtube <query> -- Returns the first YouTube search result for <query>."""
     key = bot.config.get("api_keys", {}).get("google")
 
@@ -118,7 +112,7 @@ def youtube(inp, bot=None):
 
     video_id = request['items'][0]['id']['videoId']
 
-    return get_video_description(key,video_id)
+    return '{} - {}'.format(get_video_description(db, input.chan, key,video_id),video_url.format(video_id))
 
 
 @hook.command('ytime')
