c1de855ae91dc11ddac595cb6c7a55842de2b2fe
Scott Steiner <nothingfinerthanscottsteiner@gmail.com>
2017-08-29 21:32:13 -0400

    Fix for streams with no status page, like VLC

--- plugins/streaminfo.py
+++ plugins/streaminfo.py
@@ -30,37 +30,39 @@ def getinfo(streams, viewers_colors):
 
 		status = '{}/{}?mount=/{}'.format(streams[stream_name]['root'], streams[stream_name]['status'], streams[stream_name]['stream'])
 		try:
-			if streams[stream_name]['status'] == 'status-json.xsl':
-				info = http.get_json(status)['icestats']
-				viewers_current = int(info['source']['listeners'])
-			elif streams[stream_name]['status'] == '':
-				raise Exception('\x0307May Be Live \x0304Status Page Not Found')
+			if streams[stream_name]['status'] == '':
+				viewers = '\x0307May Be Live \x0304Status Page Not Found'
 			else:
-				info = http.get_xml(status).text
-				viewers_current = int(info.split(',')[9])
+				if streams[stream_name]['status'] == 'status-json.xsl':
+					info = http.get_json(status)['icestats']
+					viewers_current = int(info['source']['listeners'])
+				else:
+					info = http.get_xml(status).text
+					viewers_current = int(info.split(',')[9])
 
-			extra_info = ''
-			if 'extra_info' in streams[stream_name]:
-				extra_info = ' | {}'.format(streams[stream_name]['extra_info'])
-			if viewers_current == 0:
-				status_code = urllib2.urlopen(stream_url, timeout=2).getcode()
-				if status_code != 200: raise Exception('Stream not found')
+				extra_info = ''
+				if 'extra_info' in streams[stream_name]:
+					extra_info = ' | {}'.format(streams[stream_name]['extra_info'])
+				if viewers_current == 0:
+					status_code = urllib2.urlopen(stream_url, timeout=2).getcode()
+					if status_code != 200: raise Exception('Stream not found')
 
-			if viewers_max == '???':
-				viewers_color = '03'
-			else:
-				viewers_color = '04'
-				viewers_percent = (100 * viewers_current) / viewers_max
-				for i in sorted(viewers_colors.items(), reverse=True):
-					if (viewers_percent >= int(i[0])):
-						viewers_color = i[1]
-						break
-			
-			viewers = '\x03{}{}/{}\x03{}'.format(viewers_color, viewers_current, viewers_max, extra_info)
+				if viewers_max == '???':
+					viewers_color = '03'
+				else:
+					viewers_color = '04'
+					viewers_percent = (100 * viewers_current) / viewers_max
+					for i in sorted(viewers_colors.items(), reverse=True):
+						if (viewers_percent >= int(i[0])):
+							viewers_color = i[1]
+							break
+
+				viewers = '\x03{}{}/{}\x03{}'.format(viewers_color, viewers_current, viewers_max, extra_info)
 			stream_url = '\x1f\x0311{}\x03\x1f'.format(stream_url)
 
 			output[stream_name] = [stream_name, stream_url, viewers]
 		except Exception as e:
 			error_info = '\x0304NOT LIVE [{}: {}]'.format(e, e.__doc__)
-			print(formatting.output('', [], 'streaminfo', [stream_name, stream_url, error_info]))
+			print(error_info)
+#			print(formatting.output('', [], 'streaminfo', [stream_name, stream_url, error_info]))
 	return(output)
